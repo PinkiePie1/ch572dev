8b499ca52e70ab96092a7c32cd170e635dd782ab
Harchimemedes <474890266@qq.com>
2025-05-28 03:26:49 +0800

    分离子项目

---
+++ ch572.lua
@@ -0,0 +1,100 @@
+
+-- 设置交叉编译工具链
+set_toolchains("wch-riscv-gcc")
+
+toolchain("wch-riscv-gcc")
+    set_kind("standalone")
+    set_sdkdir("/mnt/c/MRStoolChain/RISC-V Embedded GCC12")
+    set_toolset("cc", "riscv-wch-elf-gcc")
+    set_toolset("as", "riscv-wch-elf-gcc")
+    set_toolset("ld", "riscv-wch-elf-gcc")
+    set_toolset("ar", "riscv-wch-elf-ar")
+    set_toolset("objcopy", "riscv-wch-elf-objcopy")
+    set_toolset("size", "riscv-wch-elf-size")
+    set_toolset("objdump", "riscv-wch-elf-objdump")
+toolchain_end()
+
+option("ldfile")
+	set_default("CH572Libs/Ld/Link.ld")
+	set_showmenu(true)
+option_end()
+
+
+target("ch572")
+	set_kind("static")
+
+	--库文件
+	add_files("CH572libs/StdPeriphDriver/*.c")
+	add_files("CH572libs/StdPeriphDriver/*.a")
+	add_files("CH572libs/Startup/startup_CH572.S")
+	add_files()
+	add_includedirs(
+	    "./",
+	    "CH572libs/StdPeriphDriver/inc",
+	    "CH572libs/Startup",
+	    "CH572libs/RVMSIS",
+	    {public = true}
+	    )
+	    
+	local arch_flags = {
+	    "-march=rv32imc_zba_zbb_zbc_zbs_xw",
+	    "-mabi=ilp32",
+	    "-mcmodel=medany",
+	    "-msmall-data-limit=8",
+	    "-msave-restore",
+	    "-fmax-errors=20",
+	    "-fmessage-length=0",
+	    "-fsigned-char",
+	    "-ffunction-sections",
+	    "-fdata-sections",
+	    "-fno-common",
+	    "-Os"
+	}
+
+	
+
+	add_ldflags(
+	    table.unpack(arch_flags),
+	    "-nostartfiles",
+	    "-Xlinker --gc-sections",
+	    "-Xlinker --print-memory-usage",
+	    "-Wl,-Map=build/ch572_blink.map",
+	    "--specs=nano.specs",
+	    "--specs=nosys.specs",
+	    "-Wl,--wrap=memcpy",
+	    "-LCH572libs/StdPeriphDriver",
+	    "-L../",
+	    "-lprintf",
+	    "-T", "$(ldfile)"
+	)
+	
+	set_policy("check.auto_ignore_flags", false)
+
+	add_cflags(table.unpack(arch_flags))
+	add_asflags(table.unpack(arch_flags), "-x", "assembler-with-cpp")
+
+	
+
+
+-- 生成bin和hex文件
+rule("generateAll")
+    after_build(function (target,opt)
+        import("core.tool.toolchain")
+        import("utils.progress")
+        os.mkdir("objs")
+        os.vrunv(target:tool("objcopy"), {
+            "-O", "binary",
+            "-S", target:targetfile(),
+            path.join("objs", target:name() .. ".bin")
+        })
+        progress.show(opt.progress, "${color.build.object}generated %s", target:name() .. ".bin")
+
+        os.vrunv(target:tool("objcopy"), {
+            "-O", "ihex",
+            "-S", target:targetfile(),
+            path.join("objs", target:name() .. ".hex")
+        })
+        progress.show(opt.progress, "${color.build.object}generated %s", target:name() .. ".hex")
+
+    end)
+
---
+++ examples/blink/xmake.lua
@@ -0,0 +1,17 @@
+target("blink")
+    set_kind("binary")
+    set_toolchains("wch-riscv-gcc")
+    add_deps("ch572")
+    add_rules("generateAll")
+
+    -- 添加源文件
+    add_files("*.c")
+
+    -- 添加头文件搜索路径
+    add_includedirs(
+        "./"
+    )
+    
+
+--  add_defines("DEBUG=1") --这里放预处理define
+
