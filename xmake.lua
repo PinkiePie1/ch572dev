a550192da96d7979ea988fad994e9b92e809a7aa
Harchimemedes <474890266@qq.com>
2025-05-27 23:15:01 +0800

    清理585部分

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 -- 设置交叉编译工具链
-set_toolchains("riscv-gcc")
+set_toolchains("wch-riscv-gcc")
 
-toolchain("riscv-gcc")
+toolchain("wch-riscv-gcc")
     set_kind("standalone")
     set_sdkdir("/mnt/c/MRStoolChain/RISC-V Embedded GCC12")
     set_toolset("cc", "riscv-wch-elf-gcc")
@@ -13,184 +13,96 @@ toolchain("riscv-gcc")
     set_toolset("objdump", "riscv-wch-elf-objdump")
 toolchain_end()
 
--- 构建目标
-target("ch585")
-    set_kind("binary")
-    set_toolchains("riscv-gcc")
-    set_targetdir("objs")
+target("ch572")
+	set_kind("static")
 
-    add_rules("c.unity_build")
+	--库文件
+	add_files("CH572libs/StdPeriphDriver/*.c")
+	add_files("CH572libs/Startup/startup_CH572.S")
+	add_includedirs(
+	    "./",
+	    "CH572libs/StdPeriphDriver/inc",
+	    "CH572libs/Startup",
+	    "CH572libs/RVMSIS",
+	    {public = true}
+	    )
+	    
+	local arch_flags = {
+	    "-march=rv32imc_zba_zbb_zbc_zbs_xw",
+	    "-mabi=ilp32",
+	    "-mcmodel=medany",
+	    "-msmall-data-limit=8",
+	    "-msave-restore",
+	    "-fmax-errors=20",
+	    "-fmessage-length=0",
+	    "-fsigned-char",
+	    "-ffunction-sections",
+	    "-fdata-sections",
+	    "-fno-common",
+	    "-Os"
+	}
 
-    -- 添加源文件
-    add_files("*.c")
-    add_files("CH585Libs/StdPeriphDriver/*.c")
-    add_files("CH585Libs/Startup/startup_CH585.S")
+	add_ldflags(
+	    table.unpack(arch_flags),
+	    "-nostartfiles",
+	    "-Xlinker --gc-sections",
+	    "-Xlinker --print-memory-usage",
+	    "-Wl,-Map=build/ch572_blink.map",
+	    "--specs=nano.specs",
+	    "--specs=nosys.specs",
+	    "-Wl,--wrap=memcpy",
+	    "-LCH572libs/StdPeriphDriver",
+	    "-L../",
+	    "-lprintf",
+	    "-lISP572",
+	    {force = true}
+	)
 
-    -- 添加头文件搜索路径
-    add_includedirs(
-        "./",
-        "CH585Libs/StdPeriphDriver/inc",
-        "CH585Libs/Startup",
-        "CH585Libs/RVMSIS"
-    )
+	set_policy("check.auto_ignore_flags", false)
 
-    -- 设置链接脚本
-    set_policy("build.merge_archive", false) -- 防止 .a 自动展开
-    add_ldflags("-TCH585Libs/Ld/Link.ld", {force = true})
-
-    -- 架构和优化
-    local arch_flags = {
-        "-march=rv32imc_zba_zbb_zbc_zbs_xw",
-        "-mabi=ilp32",
-        "-mcmodel=medany",
-        "-msmall-data-limit=8",
-        "-msave-restore",
-        "-fmax-errors=20",
-        "-fmessage-length=0",
-        "-fsigned-char",
-        "-ffunction-sections",
-        "-fdata-sections",
-        "-fno-common",
-        "-Os"
-    }
-
-    set_policy("check.auto_ignore_flags", false)
-
-    add_cflags(table.unpack(arch_flags))
-    add_asflags(table.unpack(arch_flags), "-x", "assembler-with-cpp")
-    -- add_defines("__GNUC__") -- 或你用到的 C_DEFS 宏
-
-    -- 设置链接参数
-    add_ldflags(
-        table.unpack(arch_flags),
-        "-nostartfiles",
-        "-Xlinker", "--gc-sections",
- --       "-Xlinker", "--print-memory-usage",
- --       "-Wl,-Map=objs/ch585.map",
-        "--specs=nano.specs",
-        "--specs=nosys.specs",
-        "-Wl,--wrap=memcpy",
-        "-LCH585Libs/BLELIB",
-        "-LCH585Libs/StdPeriphDriver",
-        "-L../",
-        "-lprintf",
-        "-lCH58xBLE",
-        "-lISP585",
-        {force = true}
-    )
+	add_cflags(table.unpack(arch_flags))
+	add_asflags(table.unpack(arch_flags), "-x", "assembler-with-cpp")
 
 
-    -- 生成 bin 和 hex
-    after_build(function (target)
+-- 生成bin和hex文件
+rule("generateAll")
+    after_build(function (target,opt)
         import("core.tool.toolchain")
-        -- 加载你刚才定义的 riscv-gcc toolchain
-        local gcc = target:tool("cc")
-        local objpath , _ = path.filename(gcc):gsub("gcc", "objcopy")
-        local objcopy = path.join(path.directory(gcc), objpath)
+        import("utils.progress")
         os.mkdir("objs")
-        os.execv(objcopy, {
+        os.vrunv(target:tool("objcopy"), {
             "-O", "binary",
             "-S", target:targetfile(),
             path.join("objs", target:name() .. ".bin")
         })
+        progress.show(opt.progress, "${color.build.object}generated %s", target:name() .. ".bin")
+
+        os.vrunv(target:tool("objcopy"), {
+            "-O", "ihex",
+            "-S", target:targetfile(),
+            path.join("objs", target:name() .. ".hex")
+        })
+        progress.show(opt.progress, "${color.build.object}generated %s", target:name() .. ".hex")
+
     end)
 
 
-target("CH572libs")
-    set_kind("static")
-    add_files("CH572libs/StdPeriphDriver/*.c")
-    add_files("CH572libs/Startup/startup_CH572.S")
-    
-
-    add_includedirs(
-        "./",
-        "CH572libs/StdPeriphDriver/inc",
-        "CH572libs/Startup",
-        "CH572libs/RVMSIS"
-    )
-
-
-target("ch572_blink")
+target("blink")
     set_kind("binary")
-    set_toolchains("riscv-gcc")
-    add_deps("CH572libs")
-
- --   add_rules("c.unity_build")
+    set_toolchains("wch-riscv-gcc")
+    add_deps("ch572")
+    add_rules("generateAll")
 
     -- 添加源文件
-    add_files("ch572_examples/blink/*.c")
-    add_files("CH572libs/StdPeriphDriver/*.c")
-    add_files("CH572libs/Startup/startup_CH572.S")
+    add_files("examples/blink/*.c")
 
     -- 添加头文件搜索路径
     add_includedirs(
-        "./ch572_examples/blink",
-        "CH572libs/StdPeriphDriver/inc",
-        "CH572libs/Startup",
-        "CH572libs/RVMSIS"
+        "./examples/blink"
     )
 
     -- 设置链接脚本
     set_policy("build.merge_archive", false) -- 防止 .a 自动展开
     add_ldflags("-TCH572libs/Ld/Link.ld", {force = true})
 
-    -- 架构和优化
-    local arch_flags = {
-        "-march=rv32imc_zba_zbb_zbc_zbs_xw",
-        "-mabi=ilp32",
-        "-mcmodel=medany",
-        "-msmall-data-limit=8",
-        "-msave-restore",
-        "-fmax-errors=20",
-        "-fmessage-length=0",
-        "-fsigned-char",
-        "-ffunction-sections",
-        "-fdata-sections",
-        "-fno-common",
-        "-Os"
-    }
-
-    set_policy("check.auto_ignore_flags", false)
-
-    add_cflags(table.unpack(arch_flags))
-    add_asflags(table.unpack(arch_flags), "-x", "assembler-with-cpp")
-    -- add_defines("__GNUC__") -- 或你用到的 C_DEFS 宏
-
-    -- 设置链接参数
-    add_ldflags(
-        table.unpack(arch_flags),
-        "-nostartfiles",
-        "-Xlinker --gc-sections",
-        "-Xlinker --print-memory-usage",
-        "-Wl,-Map=build/ch572_blink.map",
-        "--specs=nano.specs",
-        "--specs=nosys.specs",
-        "-Wl,--wrap=memcpy",
-        "-LCH572libs/StdPeriphDriver",
-        "-L../",
-        "-lprintf",
-        "-lISP572",
-        {force = true}
-    )
-
-
-    -- 生成 bin 和 hex
-    after_build(function (target)
-        import("core.tool.toolchain")
-        -- 加载你刚才定义的 riscv-gcc toolchain
-        local gcc = target:tool("cc")
-        local objpath , _ = path.filename(gcc):gsub("gcc", "objcopy")
-        local objcopy = path.join(path.directory(gcc), objpath)
-        os.mkdir("objs")
-        os.execv(objcopy, {
-            "-O", "binary",
-            "-S", target:targetfile(),
-            path.join("objs", target:name() .. ".bin")
-        })
-
-        os.execv(objcopy, {
-            "-O", "ihex",
-            "-S", target:targetfile(),
-            path.join("objs", target:name() .. ".hex")
-        })
-    end)
+ --   add_defines("DEBUG=1") --这里放预处理define 
