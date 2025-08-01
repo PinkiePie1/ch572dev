639bd8fe7e5da6a0cee117a115be7cb157f29f1f
Harchimemedes <474890266@qq.com>
2025-08-01 21:41:22 +0800

    更新unitybuild加速

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,5 @@
 set_policy("build.intermediate_directory", false)
+add_rules("c.unity_build")
 
 function check_ld()
 	local local_ld = os.scriptdir() .. "/link.ld"
@@ -9,6 +10,7 @@ function check_ld()
 		add_cflags("-ICH572libs/Ld")
 	end
 end
+
 
 includes("ch572.lua")
 
